8f53de7c7a5cb5db2a5549cab7d1d7a76902a095
prachi9124 <prachianad321@gmail.com>
2023-11-05 02:24:12 +0530

    Chef And Brain Speed Code Done

---
+++ Practice C++/chef_n_brain_speed.cpp
@@ -0,0 +1,17 @@
+//Chef And Brain Speed
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x, y;
+	cin>> x>> y;
+	if(x >= y){
+	    cout<<"NO"<<endl;
+	}
+	else{
+	    cout<<"YES"<<endl;
+	}
+	return 0;
+}
