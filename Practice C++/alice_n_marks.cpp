fc229b66f224485269913b1c7f84ff1283dc7919
prachi9124 <prachianad321@gmail.com>
2023-11-05 02:26:37 +0530

    Alice And Marks Code Done

---
+++ Practice C++/alice_n_marks.cpp
@@ -0,0 +1,16 @@
+//Alice And Marks
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x, y;
+	cin>> x>> y;
+	if(x>=(2*y)){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
