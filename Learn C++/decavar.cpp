b6a929b29a0f025ac2ac05981b49cf134c098dfd
prachi9124 <prachianad321@gmail.com>
2023-11-05 21:04:22 +0530

    Declaring A Variable Code Done

---
+++ Learn C++/decavar.cpp
@@ -0,0 +1,11 @@
+//Declaring A Variable
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int Number = 19;     //Update the blanks with the value assigned to Number
+  cout << Number;      //Output the variable to the console
+  
+  return 0;
+}
