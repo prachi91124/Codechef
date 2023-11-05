b217fa184d66b889e675cbd483ee6b034f597844
prachi9124 <prachianad321@gmail.com>
2023-11-05 20:30:47 +0530

    Off By One Code Done

---
+++ Starters Contest Codes/offbyone.cpp
@@ -0,0 +1,13 @@
+//Off By One
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a, b;
+	int d = 1;
+	cin>>a>>b;
+	int sum = a+b;
+	cout<<sum<<d<<endl;
+	return 0;
+}
