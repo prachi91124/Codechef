ad3285ca8fd28212829c93eb1be17091e2a26d46
prachi9124 <prachianad321@gmail.com>
2023-11-05 21:07:05 +0530

    Area Of Rectangle Code Done

---
+++ Learn C++/arofrect.cpp
@@ -0,0 +1,15 @@
+//Area Of Rectangle
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int length = 45;
+  int width = 76;
+  int area = length * width;
+  cout << "The Area of the given rectangle is ";
+  cout << area; 
+
+  return 0;
+}
