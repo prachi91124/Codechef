dfb39d70476f030913b2f5eaa98774586d609a83
prachi9124 <prachianad321@gmail.com>
2023-11-05 21:08:06 +0530

    Double Datatype Code Done

---
+++ Learn C++/doubledatatype.cpp
@@ -0,0 +1,15 @@
+//Double Datatype
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  double pi = 3.14;
+  double radius = 8.9;      //radius has to be declared as a 'double'
+  double area = pi * radius * radius;
+  cout << "The Area of the given Circle is ";
+  cout << area;
+  
+  return 0;
+}
