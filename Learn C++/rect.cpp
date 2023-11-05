9d22a48d95500e6808594e3fca414f94dab3d3c9
prachi9124 <prachianad321@gmail.com>
2023-11-05 20:51:26 +0530

    Area And Perimeter Of Rectangle Code Done

---
+++ Learn C++/rect.cpp
@@ -0,0 +1,12 @@
+//Area And Perimeter Of Rectangle
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  cout << 11 * 13 <<endl; //Replace the _ with correct digits
+  cout << 2 * ( 11 + 13 );
+
+  return 0;
+}
