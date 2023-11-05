36545c1aa9955acc6be77731a53fb28de8d43ae5
prachi9124 <prachianad321@gmail.com>
2023-11-05 20:32:37 +0530

    Fitness Code Done

---
+++ Basic Math Using C++/fitness.cpp
@@ -0,0 +1,15 @@
+//Fitness
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    cout<<10*x<<endl;
+	}
+	return 0;
+}
