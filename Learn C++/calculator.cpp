c60c7f35ea4b28c3d8c88e9899e3f8bb262547d3
prachi9124 <prachianad321@gmail.com>
2023-11-06 13:13:21 +0530

    Calculator Code Done

---
+++ Learn C++/calculator.cpp
@@ -0,0 +1,17 @@
+//Calculator
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int a, b;
+  int sum;
+  int diff;
+  cin >> a;
+  cin >> b;
+  sum = a+b;
+  diff = a-b;
+  cout<<"Sum is: "<<sum<<endl<<"Difference is: "<<diff<<endl;
+  return 0;
+}
