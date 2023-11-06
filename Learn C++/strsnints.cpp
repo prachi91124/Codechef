137b747121661c1f54ff17019431283f2aa38b74
prachi9124 <prachianad321@gmail.com>
2023-11-06 12:36:29 +0530

    Strings And Integers Code Done

---
+++ Learn C++/strsnints.cpp
@@ -0,0 +1,16 @@
+//Strings And Integers
+
+//We need to output 2569 using string concatenation.
+//The code below is incorrect - Debug the code to solve the problem
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+
+  string num1 = "25";
+  string num2 = "69";
+  cout << num1 + num2;
+
+  return 0;
+}
