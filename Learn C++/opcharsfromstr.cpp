8718531de522a15277923b0cb391c18165f674fc
prachi9124 <prachianad321@gmail.com>
2023-11-06 12:59:40 +0530

    Output Chars From Str Code Done

---
+++ Learn C++/opcharsfromstr.cpp
@@ -0,0 +1,12 @@
+//Outputting characters from The String
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string word;
+    word = "Programming";
+    cout<<word[2]<<endl<<word[1]<<endl;
+    return 0;
+}
