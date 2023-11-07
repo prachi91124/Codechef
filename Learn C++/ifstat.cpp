dff7e6b087495362547a3f0cdcdeb9f7102e3eb0
prachi9124 <prachianad321@gmail.com>
2023-11-07 15:07:33 +0530

    If Statement Code Done

---
+++ Learn C++/ifstat.cpp
@@ -0,0 +1,14 @@
+//If Statement
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cin>>a>>b;
+    if(a>b){
+        cout<<"Coding is Fun!";
+    }
+    return 0;
+}
