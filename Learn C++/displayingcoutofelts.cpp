8b2e73f7a822935a44420549ab7a901a18c96807
prachi9124 <prachianad321@gmail.com>
2023-11-16 12:52:47 +0530

    Displaying Count Of Elements Code Done

---
+++ Learn C++/displayingcoutofelts.cpp
@@ -0,0 +1,10 @@
+//Displaying Count Of Elements
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int myNumbers[6]={10,20,30,40,50,60};
+    cout<<sizeof(myNumbers)/4;
+  return 0;
+}
