60b43f19dd43e8476f03664ee68344f70e828e80
prachi9124 <prachianad321@gmail.com>
2023-11-30 09:09:50 +0530

    Function Within A Function Cdoe Done

---
+++ Learn C++/fnwithinfn.cpp
@@ -0,0 +1,31 @@
+//Function Within A Function
+
+#include <iostream>
+#include <string>  // For using string functions
+#include <algorithm>  // For using transform function
+using namespace std;
+
+// Function to print the greeting
+void printGreeting(const string& name) {
+
+    // Convert the greeting to uppercase
+    string upperGreeting = "Hello, " + name + "!";
+    
+    // converts the string to uppercase
+    transform(upperGreeting.begin(), upperGreeting.end(), upperGreeting.begin(), ::toupper);
+
+    // Print the final result
+    
+    cout<<"Final Result: "<<upperGreeting;
+
+}
+
+int main() {
+    // The name to be used in the greeting
+    string name = "Alice";
+    
+    printGreeting(name);
+
+
+    return 0;
+}
