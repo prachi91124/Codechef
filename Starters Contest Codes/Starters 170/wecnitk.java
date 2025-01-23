867f292a8456a7f8bdcb6ef3c3947fe2f73fdd14
Prachi Anand <prachianand321@gmail.com>
2025-01-23 09:34:17 +0530

    Access Code Equality of Starters 170 solved

---
+++ Starters Contest Codes/Starters 170/wecnitk.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		
+		if(s.equals("WECNITK")){
+		    System.out.print("Welcome to Web Club!");
+		}else{
+		    System.out.print("Access denied");
+		}
+
+	}
+}
